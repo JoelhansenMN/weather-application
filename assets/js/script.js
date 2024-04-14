ef8b7731eee6da62e05b0737fd0ec95f30d16b70
JoelhansenMN <joelhansen22@gmail.com>
2024-04-14 12:36:19 -0500

    adding event listeners to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const cityName = $('#city-name');
-
+const todayForecast = $('#current-city');
 const apiKey = "d21fb8ec91ef8ad73e394a6f5fac325a";
-
+const searchForm = $('#search-form');
 
 
 
@@ -20,41 +20,66 @@ function saveCitiesToStorage(cities){
 
 //get's city's lat & lon, saves it into an object, and updates the cities array
 function getGeoAPi(){
-  const requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName.val()}&appid=${apiKey}&units=imperial`; //geocoding API for lat/lon
-  //console.log(requestURL);
-
+  const requestURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName.val()}&appid=${apiKey}`; //geocoding API for lat/lon
+  console.log(requestURL);
   fetch(requestURL)
   .then(response => {
+    
     return response.json();
   })
 
   .then(data =>{
     for (let i = 0; i < data.length; i++) {
-      const obj = data[i];
+      const object = data[i];
       let city = {
         id:crypto.randomUUID(),
-        city:obj.name,
-        long: obj.lon,
-        lat:obj.lat
+        city:object.name,
+        long: object.lon,
+        lat:object.lat
       };
-
+      
       let citiesArray = readCitiesFromStorage()
       citiesArray.push(city);
       saveCitiesToStorage(citiesArray)
-      const requestURL = `https://api.openweathermap.org/data/s.5/forecast?lat=${obj.lat}&lon=${obj.lon}&appid=${apiKey}&units=imperial`;
+      const requestURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${object.lat}&lon=${object.lon}&appid=${apiKey}`;
       return fetch(requestURL);
+      
   }
 }) 
 
 .then(response => {
   return response.json();
-}
-  
-  )
+})
+
+.then(function (data2){
+  //console.log(data2);
+  //removeDuplicates();
+  localStorage.setItem('weatherSearch', JSON.stringify(data2));
+  //todayWeather();
+  //fiveDayWeather();
+  //createSearchHistory(); //calls create search history function
+})
 
 }
 
+function todayWeather() {
+  let weatherData = JSON.parse(localStorage.getItem('weatherSearch'));
+  let today = $('<div>');
+  today.attr('id', 'today').addclass('text-black');
+  let todayForecast = $('<div>');
+  todayForecast.attr('id', 'todayForecast');
+  todaySection.addClass('border-dark');
+}
 
+
+
+
+
+searchForm.on('click','.btn', function(event){event.preventDefault()
+getGeoAPi();
+});
+
+$('#search-history')
 //fetch(`http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`)
 //.then( response=> response.json() )
 //.then(data2 =>{}

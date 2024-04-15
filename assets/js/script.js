02cc695c03309b07d6fd2b6df9a0ed7b7e7acad8
JoelhansenMN <joelhansen22@gmail.com>
2024-04-14 22:26:57 -0500

    five day forecast function in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ function getGeoAPi(){
       let citiesArray = readCitiesFromStorage()
       citiesArray.push(city);
       saveCitiesToStorage(citiesArray)
-      const requestURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${object.lat}&lon=${object.lon}&appid=${apiKey}`;
+      const requestURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${object.lat}&lon=${object.lon}&appid=${apiKey}&units=imperial`;
       console.log(requestURL);
       return fetch(requestURL);     
   }
@@ -51,35 +51,58 @@ function getGeoAPi(){
 
 .then(function (data2){
   localStorage.setItem('weatherSearch', JSON.stringify(data2));
-
+  todayForecast();
+  fiveDayForecast();
 })
 .catch(function(error){console.log(error)
 
 alert('An error has occured')})
 } 
 
+// this function gets data from API to create our daily forecast box
 function todayForecast() {
   let weatherSearch = JSON.parse(localStorage.getItem('weatherSearch'));
-  //console.log(weatherSearch);
+  console.log(weatherSearch);
   let currentCity = $('#current-city');
   let todayBox = $('<div>');
   todayBox.addClass('col-9 align-items-end text-dark').attr('id', 'todayBox'); 
   let h2 = $('<h2>');
   h2.text(`${weatherSearch.city.name} ${dayjs.unix(weatherSearch.list[0].dt).format('MM/DD/YYYY')}`).appendTo(todayBox);
   let temp = $('<p>');
-  temp.text(`${weatherSearch.list[0].main.temp}&deg;F`).appendTo(todayBox);
+  temp.text(`Temperature: ${weatherSearch.list[0].main.temp} °F`).appendTo(todayBox);
   let wind = $('<p>');
-  wind.text(`${weatherSearch.list[0].wind.speed} MPH`).appendTo(todayBox);
+  wind.text(`Wind Speed: ${weatherSearch.list[0].wind.speed} MPH`).appendTo(todayBox);
   let humidity = $('<p>');
-  humidity.text(`${weatherSearch.list[0].main.humidity} %`).appendTo(todayBox);
-  todayBox.append(currentCity);
+  humidity.text(`Humidity: ${weatherSearch.list[0].main.humidity} %`).appendTo(todayBox);
+  todayBox.appendTo(currentCity);
 } 
 
+function fiveDayForecast() {
+  let h2 = $('<h2>');
+  let mainWeek = $('.week');
+  h2.text('Five Day Forecast').appendTo('five-day');
+  let week = $('<p>');
+  week.addClass('row align-items-start justify-content-evenly').appendTo(mainWeek);
+  let weatherSearch = JSON.parse(localStorage.getItem('weatherSearch'));
+  let fiveDay = [];
+  let list = weatherSearch.list;
+  console.log(list);
+  for (let i = 1;  i < list.length; i+=8) {
+   let listObj = list[i]
+    //fiveDay.push(listObj);
+    console.log(listObj);
+  }
+  
 
+
+
+
+}
 
 searchForm.on('click','.btn', function(event){event.preventDefault()
 getGeoAPi();
-todayForecast();
+
 });
+
 
 $('#search-history')

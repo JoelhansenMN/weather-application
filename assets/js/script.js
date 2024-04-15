2c08734a755e31dcd99e5272db7eccf3e5f1fa49
JoelhansenMN <joelhansen22@gmail.com>
2024-04-14 20:02:40 -0500

    working js todays weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const cityName = $('#city-name');
-const todayForecast = $('#current-city');
+//const currentCity = $('#current-city');
 const apiKey = "d21fb8ec91ef8ad73e394a6f5fac325a";
 const searchForm = $('#search-form');
 
@@ -21,7 +21,6 @@ function saveCitiesToStorage(cities){
 //get's city's lat & lon, saves it into an object, and updates the cities array
 function getGeoAPi(){
   const requestURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName.val()}&appid=${apiKey}`; //geocoding API for lat/lon
-  console.log(requestURL);
   fetch(requestURL)
   .then(response => {
     
@@ -35,53 +34,52 @@ function getGeoAPi(){
         id:crypto.randomUUID(),
         city:object.name,
         long: object.lon,
-        lat:object.lat
-      };
-      
+        lat:object.lat,
+      }
       let citiesArray = readCitiesFromStorage()
       citiesArray.push(city);
       saveCitiesToStorage(citiesArray)
       const requestURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${object.lat}&lon=${object.lon}&appid=${apiKey}`;
-      return fetch(requestURL);
-      
+      console.log(requestURL);
+      return fetch(requestURL);     
   }
 }) 
-
 .then(response => {
   return response.json();
+
 })
 
 .then(function (data2){
-  //console.log(data2);
-  //removeDuplicates();
   localStorage.setItem('weatherSearch', JSON.stringify(data2));
-  //todayWeather();
-  //fiveDayWeather();
-  //createSearchHistory(); //calls create search history function
+
 })
+.catch(function(error){console.log(error)
 
-}
+alert('An error has occured')})
+} 
 
-function todayWeather() {
-  let weatherData = JSON.parse(localStorage.getItem('weatherSearch'));
-  let today = $('<div>');
-  today.attr('id', 'today').addclass('text-black');
-  let todayForecast = $('<div>');
-  todayForecast.attr('id', 'todayForecast');
-  todaySection.addClass('border-dark');
-}
-
-
+function todayForecast() {
+  let weatherSearch = JSON.parse(localStorage.getItem('weatherSearch'));
+  //console.log(weatherSearch);
+  let currentCity = $('#current-city');
+  let todayBox = $('<div>');
+  todayBox.addClass('col-9 align-items-end text-dark').attr('id', 'todayBox'); 
+  let h2 = $('<h2>');
+  h2.text(`${weatherSearch.city.name} ${dayjs.unix(weatherSearch.list[0].dt).format('MM/DD/YYYY')}`).appendTo(todayBox);
+  let temp = $('<p>');
+  temp.text(`${weatherSearch.list[0].main.temp}&deg;F`).appendTo(todayBox);
+  let wind = $('<p>');
+  wind.text(`${weatherSearch.list[0].wind.speed} MPH`).appendTo(todayBox);
+  let humidity = $('<p>');
+  humidity.text(`${weatherSearch.list[0].main.humidity} %`).appendTo(todayBox);
+  todayBox.append(currentCity);
+} 
 
 
 
 searchForm.on('click','.btn', function(event){event.preventDefault()
 getGeoAPi();
+todayForecast();
 });
 
 $('#search-history')
-//fetch(`http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`)
-//.then( response=> response.json() )
-//.then(data2 =>{}
-
-//)
